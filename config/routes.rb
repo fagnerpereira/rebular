14443816eb42b62fec606e5dd3164a68228e0ee3
Fagner <fagnerfpr@gmail.com>
2024-07-24 20:11:09 -0300

    add assets

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,5 @@ module Rebular
   class Routes < Hanami::Routes
     # Add your routes here. See https://guides.hanamirb.org/routing/overview/ for details.
     get "/home", to: "home.index"
-    get "/home/:id", to: "home.show"
-    get "/home/:id", to: "home.show"
   end
 end
---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./app/templates/**/*.{html,js,slim}",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
